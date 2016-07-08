853054003c4b7edeef20c264ebe49cc5b3201c5d
rsvsukumar <rsvsukumar@gmail.com>
2016-07-08 03:29:55 -0700

    8. In the footer, under the LATEST section, display the 4 most recent albums released.    Use a caching technique to prevent this query from executing every request.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 require "application_responder"
-
+require 'date'
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
-
+  before_action :most_recent_4months_album
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def most_recent_4months_album
+    @recent_albums ||= Album.where("released_on >= ? AND released_on <= ?" , Date.today - 4.months, Date.today + 4.months)
+  end
+
 end
